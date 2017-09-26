c1d71f7fdf0c7a1d74b1346b6eae588f61fcfc62
cousinst <trevor_cousins@yahoo.com>
2017-09-26 16:02:50 +0100

    Adding data repositoriy access class (empty)

---
+++ src/SFA.DAS.Activities.DataAccess/Repositories/ActivitiesRepository.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using SFA.DAS.Activities.API.Types.Enums;
+
+namespace SFA.DAS.Activities.DataAccess.Repositories
+{
+    public class ActivitiesRepository : BaseRepository, ITaskRepository
+    {
+        public TaskRepository(ActivitiesConfiguration configuration, ILog logger) : base(configuration.DatabaseConnectionString, logger)
+        { }
+
+        public async Task<IEnumerable<Activity>> GetActivitys(string ownerId)
+        {
+            
+        }
+
+        public async Task<Activity> GetActivity(string ownerId, ActivityType type)
+        {
+            
+        }
+
+        public async Task SaveActivity(Activity activity)
+        {
+            
+        }
+    }
+}
