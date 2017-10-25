74e95e87736e071034262d7b74a6017b1efeec91
Trevor Cousins <trevor_cousins@yahoo.com>
2017-10-25 15:12:23 +0100

    Activities. Sorting out elastic mapping

--- src/MessagingConsole/Program.cs
+++ src/MessagingConsole/Program.cs
@@ -4,27 +4,80 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MediatR;
+using Nest;
+using NuGet;
 
 namespace MessagingConsole
 {
     class Program
     {
+        private static ElasticClient elasticClient;
         static void Main(string[] args)
         {
-            IMediator _mediator=new Mediator();
+            var indexName = "activitiestest";
+            //var elasticSettings = new ConnectionSettings(new Uri(configuration.ElasticServerBaseUrl));
+            var elasticSettings = new ConnectionSettings(new Uri("http://localhost:9200")).DefaultIndex(indexName);
+            elasticClient = new ElasticClient(elasticSettings);
 
-            _mediator.SendAsync(new SaveActivityCommand(
-                new FluentActivity()
-                    .OwnerId(message.OwnerId)
-                    .ActivityType(Activity.ActivityType.ApprenticeChangesApproved)
-                    .DescriptionSingular($"cohort approved with {message.ProviderName}")
-                    .DescriptionPlural("cohorts approved")
-                    .PostedDateTime(message.PostedDatedTime)
-                    .Url(message.Url)
-                    .AddAssociatedThing(message.ProviderName)
-                    .AddAssociatedThings(message.Apprentices)
-                    .Object()
-            ));
+            elasticClient.DeleteIndex(indexName);
+
+            CreateIndex(indexName);
+
+            var activities = MakeSomeActivities();
+
+            //elasticClient.IndexMany(activities, "activitiestest");
+
+            foreach (var activity in activities)
+            {
+                elasticClient.Index(activity);
+            }
+
+        }
+
+        private static void CreateIndex(string indexName)
+        {
+            elasticClient.CreateIndex(indexName, i => i.Mappings(ms => ms.Map<Activity>(m => m.AutoMap())));
+        }
+
+        private static List<Activity> MakeSomeActivities()
+        {
+            List<Activity> rtn = new List<Activity>
+            {
+                MakeActivity(Activity.ActivityType.ActivityOne),
+                MakeActivity(Activity.ActivityType.ActivityOne),
+                MakeActivity(Activity.ActivityType.ActivityOne),
+                MakeActivity(Activity.ActivityType.ActivityOne),
+
+                MakeActivity(Activity.ActivityType.ActivityTwo),
+                MakeActivity(Activity.ActivityType.ActivityTwo),
+                MakeActivity(Activity.ActivityType.ActivityTwo),
+
+                MakeActivity(Activity.ActivityType.ActivityThree),
+                MakeActivity(Activity.ActivityType.ActivityThree),
+                MakeActivity(Activity.ActivityType.ActivityThree),
+                MakeActivity(Activity.ActivityType.ActivityThree),
+
+                MakeActivity(Activity.ActivityType.ActivityFour),
+                MakeActivity(Activity.ActivityType.ActivityFour),
+
+                MakeActivity(Activity.ActivityType.ActivityFive),
+                MakeActivity(Activity.ActivityType.ActivityFive),
+
+
+            };
+
+
+
+            return rtn;
+        }
+
+        private static Activity MakeActivity(Activity.ActivityType type)
+        {
+            return new FluentActivity()
+                .ActivityType(type)
+                .PostedDateTime(DateTime.Now)
+                .DescriptionSingular("desc singular")
+                .Object();
         }
     }
 }
--- src/SFA.DAS.Activities.DataAccess/Repositories/ActivitiesRepository.cs
+++ src/SFA.DAS.Activities.DataAccess/Repositories/ActivitiesRepository.cs
@@ -19,8 +19,8 @@ namespace SFA.DAS.Activities.DataAccess.Repositories
 
         public ActivitiesRepository(ActivitiesConfiguration configuration, ILog logger)
         {
-            //var elasticSettings = new ConnectionSettings(new Uri("http://localhost:9200"));
-            var elasticSettings = new ConnectionSettings(new Uri(configuration.ElasticServerBaseUrl));
+            var elasticSettings = new ConnectionSettings(new Uri("http://localhost:9200")).DefaultIndex("activities");
+            //var elasticSettings = new ConnectionSettings(new Uri(configuration.ElasticServerBaseUrl)).DefaultIndex("activities");
             _elasticClient =new ElasticClient(elasticSettings);
 
             _logger = logger;
