5edfa990ed71100b2ad976c849ba1d7ad0fb3e9d
Elliot Ritchie <elliotritchie@hotmail.com>
2017-12-18 16:09:58 +0000

    AML-1660 Removed redundant activities service

--- src/SFA.DAS.Activities.Worker/MessageProcessors/PayeSchemeCreatedMessageProcessor.cs
+++ src/SFA.DAS.Activities.Worker/MessageProcessors/PayeSchemeCreatedMessageProcessor.cs
@@ -1,6 +1,6 @@
 ﻿using System.Threading.Tasks;
+using Nest;
 using SFA.DAS.Activities.Worker.ObjectMappers;
-using SFA.DAS.Activities.Worker.Services;
 using SFA.DAS.EmployerAccounts.Events.Messages;
 using SFA.DAS.Messaging;
 using SFA.DAS.Messaging.AzureServiceBus.Attributes;
@@ -12,21 +12,19 @@ namespace SFA.DAS.Activities.Worker.MessageProcessors
     [TopicSubscription("Activity_PayeSchemeCreatedMessageProcessor")]
     public class PayeSchemeCreatedMessageProcessor : MessageProcessor<PayeSchemeCreatedMessage>
     {
-        private readonly IMessageSubscriberFactory _subscriberFactory;
         private readonly IActivityMapper _activityMapper;
-        private readonly IActivitiesService _activitiesService;
+        private readonly IElasticClient _client;
 
-        public PayeSchemeCreatedMessageProcessor(IMessageSubscriberFactory subscriberFactory, ILog logger, IActivityMapper activityMapper, IActivitiesService activitiesService)
+        public PayeSchemeCreatedMessageProcessor(IMessageSubscriberFactory subscriberFactory, ILog logger, IActivityMapper activityMapper, IElasticClient client)
             : base(subscriberFactory, logger)
         {
-            _subscriberFactory = subscriberFactory;
             _activityMapper = activityMapper;
-            _activitiesService = activitiesService;
+            _client = client;
         }
 
         protected override async Task ProcessMessage(PayeSchemeCreatedMessage message)
         {
-            await _activitiesService.AddActivity(_activityMapper.Map(message, ActivityType.PayeSchemeAdded));
+            await _client.IndexAsync(_activityMapper.Map(message, ActivityType.PayeSchemeAdded));
         }
     }
 }
