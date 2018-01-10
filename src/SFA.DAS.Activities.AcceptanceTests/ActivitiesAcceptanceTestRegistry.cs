e29dcc3c2796b4ca2c0eef31683e34f5dfe7c10d
Elliot Ritchie <elliotritchie@hotmail.com>
2018-01-10 11:40:44 +0000

    Renamed SFA.DAS.Activities.Worker config to SFA.DAS.Activities

--- src/SFA.DAS.Activities.AcceptanceTests/ActivitiesAcceptanceTestRegistry.cs
+++ src/SFA.DAS.Activities.AcceptanceTests/ActivitiesAcceptanceTestRegistry.cs
@@ -2,7 +2,6 @@
 using SFA.DAS.Activities.AcceptanceTests.Azure;
 using SFA.DAS.Activities.Client;
 using SFA.DAS.Activities.Configuration;
-using SFA.DAS.Activities.Worker;
 using SFA.DAS.NLog.Logger;
 using StructureMap;
 
@@ -10,18 +9,16 @@ namespace SFA.DAS.Activities.AcceptanceTests
 {
     public class ActivitiesAcceptanceTestRegistry : Registry
     {
-        private const string ServiceName = "SFA.DAS.Activities.Worker";
+        private const string ServiceName = "SFA.DAS.Activities";
         private const string Version = "1.0";
 
         public ActivitiesAcceptanceTestRegistry()
         {
-            var environmentConfig = new EnvironmentConfiguration { EnvironmentName = "AT" };
-            var config = ConfigurationHelper.GetConfiguration<ActivitiesWorkerConfiguration>(ServiceName, Version);
+            var config = ConfigurationHelper.GetConfiguration<ActivitiesAcceptanceTestsConfiguration>(ServiceName, Version);
 
             IncludeRegistry<ActivitiesRegistry>();
             IncludeRegistry<ActivitiesClientRegistry>();
             For<IElasticConfiguration>().Use(config);
-            For<IEnvironmentConfiguration>().Use(environmentConfig);
             For<ILog>().Use(c => new NLogLogger(c.ParentType, null, null)).AlwaysUnique();
             For<IMessageServiceBusConfiguration>().Use(config);
             For<IAzureTopicMessageBus>().Use(new AzureTopicMessageBus(config.MessageServiceBusConnectionString, ""));
